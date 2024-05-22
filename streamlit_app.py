3f2735378109779f453de15a41765f826fa795dc
nandhakumar158 <142503309+nandhakumar158@users.noreply.github.com>
2024-05-22 13:17:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
 import streamlit
 streamlit.title('My Parents new Healthy Diner')
-streamlit.header('Welcome ')
-streamlit.text('Kate ')
+streamlit.header('🥣 Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avacado Toast')
+streamlit.header('🥣 Breakfast Menu')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
