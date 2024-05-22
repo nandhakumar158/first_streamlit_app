b6af0938401bf7088a17089528754bfcaefe40d3
nandhakumar158 <142503309+nandhakumar158@users.noreply.github.com>
2024-05-22 13:35:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,6 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),[])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
-streamlit.dataframe(fruits_to_show)
+streamlit.dataframe(my_fruit_list)
 
 ## *** Looks much better! They should be able to select by fruits. Multiple selection is also permitted.***
